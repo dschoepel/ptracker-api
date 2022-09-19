00e568a735022fd6f41dc061a892cb961222f5ea
Dave J Schoepel <dave@theschoepels.com>
2022-09-19 17:42:39 -0500

    Path update again

--- src/v1/middleware/index.js
+++ src/v1/middleware/index.js
@@ -1,5 +1,5 @@
 const authJwt = require("./authJwt");
-const verifySignUp = require("../middleware/verifySignUp");
+const verifySignUp = require("../../v1/middleware/verifySignup");
 const upload = require("./upload");
 module.exports = {
   authJwt,
