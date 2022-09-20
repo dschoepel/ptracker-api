52e4ef51aabdfc5afcec66894b49be31ed4f427c
Dave J Schoepel <dave@theschoepels.com>
2022-09-19 19:39:39 -0500

    Print dirname path

--- src/v1/utils/swagger.js
+++ src/v1/utils/swagger.js
@@ -1,6 +1,7 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const path = require("path");
+console.log("Swagger direname = ", __dirname);
 
 // Basic Meta information about this API
 const options = {
